23b6f450219a6654a21fe4f66197c9b8652263b0
Andesson lins <andessonlins93@gmail.com>
2019-01-23 22:20:07 -0300

    teste

---
+++ code.js
@@ -0,0 +1,37 @@
+function baskra() {
+    console.log(document.getElementById('a').Value)
+     
+    
+}
+/*
+function validaNum(a) {
+    var ok = new Boolean;
+    
+    if (a>0) {
+        ok = true;
+    }else{
+        ok = false;
+        alert("'A' tem que ser maior que zero");
+    }
+    
+    return ok;
+    
+}
+
+
+function validaDelta(a,b,c) {
+   var delta;
+   var ok = new Boolean(true);
+    if(validaNum()){
+        delta = ((b^2)-(4*a*c));
+            if (delta > 0) {
+            ok = true;
+            }else{
+                    alert("delta é igual a 0");
+                }
+        
+    }
+    return 
+}
+
+
