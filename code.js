5cc7f3564649a22b448652af576b137473630f9d
Andesson lins <andessonlins93@gmail.com>
2019-01-29 23:22:50 -0300

    OB

--- code.js
+++ code.js
@@ -1,44 +1,18 @@
+import * as validacao from './valida';
 
 var a = 2;
 var b= 5;
 var c= 1; 
 
-validaA(a);
 
-function validaA() {
+
+
+function bhaskara() {
+    var X;
+
+    //x=-b+();
+
     
-    var valida = new Boolean
-    if (a > 0 ) {
-     valida= true;
-     console.log('A é maior que Zero');
-    
-    }
-     else{
-        console.log('A tem que ser maior que Zero');
-        valida = false;
-    }
-
-    return valida;
-}
-
-
-function validaDelta() {
-    var valorDelta;
-    var valida = new Boolean();
-
-    if(validaDelta()){
-
-        valorDelta = ((b*2)-(4*a*c));
-
-        if(valorDelta > 1){
-            console.log('true'+ valorDelta);
-            valida = true;
-        }else{
-            valida = false;
-            delta = console.log('Valor de Delta é menor que 1');
-
-        }
-    }
 }
 
 function delta() {
