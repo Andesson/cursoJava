e7dde21465ba9fa1937d73612790679ca7ee9430
Andesson lins <andessonlins93@gmail.com>
2019-01-29 22:53:26 -0300

    asdasasd

--- code.js
+++ code.js
@@ -1,37 +1,54 @@
-function baskra() {
-    console.log(document.getElementById('a').Value)
-     
+
+var a = 2;
+var b= 5;
+var c= 1; 
+
+validaA(a);
+
+function validaA() {
     
+    var valida = new Boolean
+    if (a > 0 ) {
+     valida= true;
+     console.log('A é maior que Zero');
+    
+    }
+     else{
+        console.log('A tem que ser maior que Zero');
+        valida = false;
+    }
+
+    return valida;
 }
-/*
-function validaNum(a) {
-    var ok = new Boolean;
-    
-    if (a>0) {
-        ok = true;
+
+
+function validaDelta() {
+    var valorDelta;
+    var valida = new Boolean();
+
+    if(validaDelta()){
+
+        valorDelta = ((b*2)-(4*a*c));
+
+        if(valorDelta > 1){
+            console.log('true'+ valorDelta);
+            valida = true;
+        }else{
+            valida = false;
+            delta = console.log('Valor de Delta é menor que 1');
+
+        }
+    }
+}
+
+function delta() {
+    if(validaDelta()){
+        console.log('ok');
+
     }else{
-        ok = false;
-        alert("'A' tem que ser maior que zero");
+        console.log('erro')
     }
-    
-    return ok;
     
 }
 
-
-function validaDelta(a,b,c) {
-   var delta;
-   var ok = new Boolean(true);
-    if(validaNum()){
-        delta = ((b^2)-(4*a*c));
-            if (delta > 0) {
-            ok = true;
-            }else{
-                    alert("delta é igual a 0");
-                }
-        
-    }
-    return 
-}
-
-
+delta();
